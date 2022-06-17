5964cc97b01ff820f24a2f703a3b982a9d3784a2
Gattopandacorno <gazzotti.caterina@gmail.com>
2022-06-17 11:15:01 +0200

    ajax pt1

--- core/cart/cart.py
+++ core/cart/cart.py
@@ -7,3 +7,14 @@ class Cart():
         if 'skey' not in request.session:
             cart = self.session['skey'] = {'num': 12344 } 
             self.cart = cart
+            if 'skey' not in request.session:
+                cart = self.session['skey'] = {'number': 1013483} 
+                self.cart = cart
+
+    def add(self, product):
+        product_id = product.id
+
+        if product_id not in self.cart:
+            self.cart[product_id] = {'price': product.price}
+        
+        self.session.modified = True
--- core/cart/views.py
+++ core/cart/views.py
@@ -1,6 +1,5 @@
-import sys
-from django.shortcuts import render
-import sys
+from django.shortcuts import get_object_or_404, render
+from django.http import JsonResponse
 
 from .cart import Cart
 from store.models import  Product
@@ -10,7 +9,16 @@ def cart(request):
     return {'cart': Cart(request)}
 
 def cart_summary(request):
-    products = Product.objects.all() # TODO: taking all the products
+    products = Product.objects.all() # TODO: not all the products
     ctx = { 'products': products,
             }
-    return render(request, 'store/cart/summary.html', context=ctx)
+    return render(request, 'store/cart/summary.html', {})
+
+def cart_add(request):
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        product = get_object_or_404(Product, id=product_id)
+        cart.add(product=product)
+        response = JsonResponse({'test': 'data'})
+        return response
