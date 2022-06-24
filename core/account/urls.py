50a2fe19181993cbd27bd6045c6b7ea83c6ea83c
Gattopandacorno <gazzotti.caterina@gmail.com>
2022-06-24 13:37:55 +0200

    login and logout

--- core/account/urls.py
+++ core/account/urls.py
@@ -9,6 +9,7 @@ from . import views
 app_name = 'account'
 
 urlpatterns = [
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('login/', auth_views.LoginView.as_view(template_name='account/login.html', form_class=UserLoginForm), name='login'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('registration/', views.registration, name='registration'),
