b224b0a427a35c1534b4ad81fc1933cba5b0b742
Gattopandacorno <gazzotti.caterina@gmail.com>
2022-08-08 10:35:54 +0200

    user.is_normal replace a big if condition

--- core/account/models.py
+++ core/account/models.py
@@ -67,4 +67,9 @@ class UserBase(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.username
+
+    def is_normal(self):
+        if self.is_staff or self.is_seller:
+            return False
+        return True
     
